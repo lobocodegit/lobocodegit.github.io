48b8b6775f5d1ea858821a07284966b067c7d658
lobocodegit <pawel@lobocode.com>
2025-01-06 09:49:01 +0100

    Assets

--- form_contact.php
+++ form_contact.php
@@ -2,9 +2,12 @@
     $to = "pawel.reszka@karpul.pl"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $name = $_POST['name'];
+	$org_subject = $_POST['subject'];
+	
+	
     $subject = "Form submission";
     $subject2 = "Copy of your form submission";
-    $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message = $name . " wrote the following:" . "\n\n" . $_POST['message'] . "\n\n" . " this was the subject:" . "\n\n" . $org_subject;
 
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
